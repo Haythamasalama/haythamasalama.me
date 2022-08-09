0cc48cfb36c49ef1a4e23dece574b80833241bd3
Haytham Salama <haythamasalama@gmail.com>
2022-08-09 22:03:12 +0200

    chore: setup project

---
+++ app.config.ts
@@ -0,0 +1,70 @@
+export default defineAppConfig({
+  title: 'Haytham A. Salama  | Software Engineer',
+  description: 'Full-Stack Engineer - Laravel, Vuejs, Nuxtjs, Tawillindcss',
+  keywords: 'Haytham Salama, Software Engineer, Full-Stack Developer, Full-Stack Engineer, Software Engineering',
+  author: {
+    name: 'Haytham A. Salama'
+  },
+  menus: {
+    header: [
+      {
+        name: 'home',
+        path: '/'
+      },
+      {
+        name: 'about',
+        path: '/about'
+      },
+      {
+        name: 'projects',
+        path: '/projects'
+      },
+      {
+        name: 'articles',
+        path: '/articles'
+      },
+      {
+        name: 'Tools',
+        path: '/tools'
+      },
+      {
+        name: 'uses',
+        path: '/uses'
+      }
+    ],
+    footer: [
+      {
+        name: 'home',
+        path: '/'
+      },
+      {
+        name: 'about',
+        path: '/about'
+      },
+      {
+        name: 'projects',
+        path: '/projects'
+      },
+      {
+        name: 'articles',
+        path: '/articles'
+      },
+      {
+        name: 'videos',
+        path: '/videos'
+      },
+      {
+        name: 'snippets',
+        path: '/snippets'
+      },
+      {
+        name: 'Tools',
+        path: '/tools'
+      },
+      {
+        name: 'uses',
+        path: '/uses'
+      }
+    ]
+  }
+});
---
+++ nuxt.config.ts
@@ -0,0 +1,29 @@
+export default defineNuxtConfig({
+  modules: ['@nuxtjs/tailwindcss', '@nuxt/content'],
+
+  content: {
+    highlight: {
+      theme: 'one-dark-pro',
+
+      preload: [
+        'html',
+        'css',
+        'bash',
+        'javascript',
+        'typescript',
+        'json',
+        'scss',
+        'php',
+        'python',
+        'sql',
+        'vue',
+        'java'
+      ]
+    }
+  },
+
+  tailwindcss: {
+    cssPath: '~/assets/css/main.css',
+    configPath: '~/tailwind.config.js'
+  }
+});
