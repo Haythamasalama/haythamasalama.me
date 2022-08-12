f94da39d8f0dbc701b9cfadec8466ff9ea3b3948
Haytham Salama <haythamasalama@gmail.com>
2022-08-13 00:10:03 +0300

    Add logo and menus

---
+++ components/Header.vue
@@ -0,0 +1,57 @@
+<script setup>
+const menus = [
+  {
+    name: "Home",
+    path: "/",
+  },
+  {
+    name: "About",
+    path: "/about",
+  },
+  {
+    name: "Projects",
+    path: "/projects",
+  },
+  {
+    name: "Blog",
+    path: "/blog",
+  },
+  {
+    name: "Vidoes",
+    path: "/vidoes",
+  },
+  {
+    name: "Tools",
+    path: "/tools",
+  },
+  {
+    name: "Resources",
+    path: "/resources",
+  },
+  {
+    name: "Uses",
+    path: "/uses",
+  },
+];
+</script>
+
+<template>
+  <div class="w-full flex justify-between items-center py-2">
+    <!-- logo -->
+    <div>
+      <img src="@/assets/images/logo.svg" />
+    </div>
+    <!-- menus -->
+    <nav>
+      <ul class="flex justify-between">
+        <li class="text-white mx-2" v-for="(menu, key) in menus" :key="key">
+          {{ menu.name }}
+        </li>
+      </ul>
+    </nav>
+    <!-- icons -->
+    <div>
+      <img src="@/assets/images/logo.svg" />
+    </div>
+  </div>
+</template>
