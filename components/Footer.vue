032a58a915f8aa0586bf5039b8fe1d3d5f398bb4
Haytham Salama <haythamasalama@gmail.com>
2022-08-13 00:16:14 +0300

    setup footer

---
+++ components/Footer.vue
@@ -0,0 +1,48 @@
+<script setup>
+const menus = [
+  {
+    name: "Home",
+    path: "/",
+  },
+  {
+    name: "About",
+    path: "/about",
+  },
+  {
+    name: "Projects",
+    path: "/projects",
+  },
+  {
+    name: "Blog",
+    path: "/blog",
+  },
+  {
+    name: "Vidoes",
+    path: "/vidoes",
+  },
+  {
+    name: "Tools",
+    path: "/tools",
+  },
+  {
+    name: "Resources",
+    path: "/resources",
+  },
+  {
+    name: "Uses",
+    path: "/uses",
+  },
+];
+</script>
+
+<template>
+  <footer class="w-full flex justify-center items-center py-2">
+    <nav>
+      <ul class="flex justify-between">
+        <li class="text-white mx-2" v-for="(menu, key) in menus" :key="key">
+          {{ menu.name }}
+        </li>
+      </ul>
+    </nav>
+  </footer>
+</template>
