a19690fef44ac952f609caaea70a49bfb37d0b16
Haytham Salama <haythamasalama@gmail.com>
2022-08-12 22:59:53 +0300

    setup tailwind.config.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,45 @@
+import { Config } from "tailwindcss";
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
+export default <Config>{
+  mode: "jit",
+  content: [
+    `components/**/*.{vue,js}`,
+    `layouts/**/*.vue`,
+    `pages/**/*.vue`,
+    `composables/**/*.{js,ts}`,
+    `plugins/**/*.{js,ts}`,
+    `App.{js,ts,vue}`,
+    `app.{js,ts,vue}`,
+  ],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        primary: "#5194FF",
+        secondary: "#7981E6",
+        gray: {
+          100: "#fafafa",
+          200: "#eaeaea",
+          300: "#999999",
+          400: "#888888",
+          500: "#24292F",
+          600: "#1B1F24",
+          700: "#333333",
+          800: "#222222",
+          900: "#111111",
+        },
+      },
+      boxShadow: {
+        DEFAULT: "0px 4px 4px rgba(0, 0, 0, 0.25)",
+      },
+      borderRadius: {
+        DEFAULT: "10px",
+      },
+      fontFamily: {
+        sans: ["IBM Plex Sans", ...fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
