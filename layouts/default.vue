15d874bf1e7a068e57ae21a0df741d2903bb54c7
Haytham Salama <haythamasalama@gmail.com>
2022-08-13 00:10:19 +0300

    setup default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,3 +1,21 @@
+<script setup>
+const route = useRoute();
+
+useHead({
+  title: route.params.title,
+  bodyAttrs: {
+    class: "bg-gray-600",
+  },
+});
+</script>
+
 <template>
-  <div></div>
+  <div class="px-12 m-auto">
+    <!-- header -->
+    <Header />
+    <!-- page content -->
+    <slot />
+    <!-- footer -->
+    <Footer />
+  </div>
 </template>
