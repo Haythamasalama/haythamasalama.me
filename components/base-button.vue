cd4dec16ccf08b9af9ebf810947fc90b1dce9bdd
Haytham Salama <haythamasalama@gmail.com>
2022-10-01 21:23:45 +0200

    feat(components): add base button component

---
+++ components/base-button.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+  const NuxtLink = resolveComponent('NuxtLink');
+
+  defineProps<Partial<{
+    title: string;
+    more?: boolean;
+    to?: string;
+  }>>();
+</script>
+
+<template>
+  <Component
+    :is="to ? NuxtLink : 'button'"
+    :to="to"
+    class="btn group"
+  >
+    <slot>
+      <span v-if="more" class="flex gap-x-0.5">
+        More <IconArrowRight />
+      </span>
+      <span v-else>{{ title }}</span>
+    </slot>
+  </Component>
+</template>
