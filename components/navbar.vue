34a13c682489f4f599a4a586cb24ff6a2fccfde4
Haytham Salama <haythamasalama@gmail.com>
2022-10-03 21:26:01 +0200

    feat(components): add navbar component

---
+++ components/navbar.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+  defineProps<{
+    menus: {
+      name: string;
+      path: string;
+    }[];
+  }>();
+</script>
+
+<template>
+  <nav>
+    <NuxtLink
+      v-for="(menu, key) in menus"
+      :key="key"
+      :to="menu.path"
+      class="hover:text-white font-medium transition-primary mb-1 lg:mb-0"
+      active-class="text-white"
+    >
+      {{ menu.name }}
+    </NuxtLink>
+  </nav>
+</template>
