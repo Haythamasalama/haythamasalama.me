89f9394ba8c7a3fc2194f9f0e96f0e3b241cbd31
Haytham A. Salama <haythamasalama@gmail.com>
2025-04-28 01:18:37 +0300

    fix: solve `tailwind.config.js` deployment

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-
-import { fontFamily } from 'tailwindcss/defaultTheme';
+const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,vue}'],
@@ -26,83 +25,83 @@ module.exports = {
         DEFAULT: '0px 4px 4px rgba(0, 0, 0, 0.25)'
       },
       fontFamily: {
-        sans: ['Inter', ...fontFamily.sans]
+        sans: ['Inter', ...defaultTheme.fontFamily.sans]
       },
       borderColor: ({ theme }) => ({
         DEFAULT: theme('colors.gray.500')
       }),
-      typography: (theme) => {
-        return {
-          DEFAULT: {
-            css: {
-              'h1 a, h2 a, h3 a, h4 a': {
-                borderBottom: 'none !important',
-                color: 'inherit',
-                fontWeight: 'inherit'
-              },
-              a: {
-                color: theme('colors.primary'),
-                textDecoration: 'none'
-              },
-              'a:hover': {
-                borderColor: theme('colors.primary')
-              },
-              'a:has(> code)': {
-                borderColor: 'transparent !important'
-              },
-              'a code': {
-                color: 'var(--tw-prose-code)',
-                borderRadius: '0.5rem',
-                padding: '0.25rem 0.375rem',
-                border: '2px solid var(--tw-prose-pre-border)'
-              },
-              'a:hover code': {
-                color: 'var(--tw-prose-links)',
-                boxShadow: 'var(--tw-ring-inset) 0 0 0 calc(2px + var(--tw-ring-offset-width)) var(--tw-ring-color)'
-              },
-              pre: {
-                borderRadius: '0.5rem',
-                shadow: 'box-shadow: 0 1px 2px 0 rgb(0 0 0 / 0.05)',
-                whiteSpace: 'pre-wrap',
-                wordBreak: 'break-word'
-              },
-              code: {
-                borderRadius: '0.5rem',
-                padding: '0.25rem 0.375rem',
-                border: '2px solid var(--tw-prose-pre-border)'
-              },
-              'code::before': {
-                content: ''
-              },
-              'code::after': {
-                content: ''
-              },
-              table: {
-                wordBreak: 'break-word'
-              }
-            }
-          },
-          primary: {
-            css: {
-              '--tw-prose-body': theme('colors.gray.100'),
-              '--tw-prose-headings': theme('colors.white'),
-              '--tw-prose-lead': theme('colors.white'),
-              '--tw-prose-links': theme('colors.white'),
-              '--tw-prose-hr': theme('colors.gray.500'),
-              '--tw-prose-quotes': theme('colors.gray.700'),
-              '--tw-prose-quote-borders': theme('colors.gray.500'),
-              '--tw-prose-captions': theme('colors.gray.500'),
-              '--tw-prose-code': theme('colors.gray.100'),
-              '--tw-prose-pre-code': theme('colors.gray.700'),
-              '--tw-prose-pre-bg': theme('colors.gray.700'),
-              '--tw-prose-pre-border': theme('colors.gray.500'),
-              '--tw-prose-th-borders': theme('colors.gray.500'),
-              '--tw-prose-td-borders': theme('colors.gray.500')
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            'h1 a, h2 a, h3 a, h4 a': {
+              borderBottom: 'none !important',
+              color: 'inherit',
+              fontWeight: 'inherit'
+            },
+            a: {
+              color: theme('colors.primary'),
+              textDecoration: 'none'
+            },
+            'a:hover': {
+              borderColor: theme('colors.primary')
+            },
+            'a:has(> code)': {
+              borderColor: 'transparent !important'
+            },
+            'a code': {
+              color: 'var(--tw-prose-code)',
+              borderRadius: '0.5rem',
+              padding: '0.25rem 0.375rem',
+              border: '2px solid var(--tw-prose-pre-border)'
+            },
+            'a:hover code': {
+              color: 'var(--tw-prose-links)',
+              boxShadow: 'var(--tw-ring-inset) 0 0 0 calc(2px + var(--tw-ring-offset-width)) var(--tw-ring-color)'
+            },
+            pre: {
+              borderRadius: '0.5rem',
+              shadow: 'box-shadow: 0 1px 2px 0 rgb(0 0 0 / 0.05)',
+              whiteSpace: 'pre-wrap',
+              wordBreak: 'break-word'
+            },
+            code: {
+              borderRadius: '0.5rem',
+              padding: '0.25rem 0.375rem',
+              border: '2px solid var(--tw-prose-pre-border)'
+            },
+            'code::before': {
+              content: '""'
+            },
+            'code::after': {
+              content: '""'
+            },
+            table: {
+              wordBreak: 'break-word'
             }
           }
-        };
-      }
+        },
+        primary: {
+          css: {
+            '--tw-prose-body': theme('colors.gray.100'),
+            '--tw-prose-headings': theme('colors.white'),
+            '--tw-prose-lead': theme('colors.white'),
+            '--tw-prose-links': theme('colors.white'),
+            '--tw-prose-hr': theme('colors.gray.500'),
+            '--tw-prose-quotes': theme('colors.gray.700'),
+            '--tw-prose-quote-borders': theme('colors.gray.500'),
+            '--tw-prose-captions': theme('colors.gray.500'),
+            '--tw-prose-code': theme('colors.gray.100'),
+            '--tw-prose-pre-code': theme('colors.gray.700'),
+            '--tw-prose-pre-bg': theme('colors.gray.700'),
+            '--tw-prose-pre-border': theme('colors.gray.500'),
+            '--tw-prose-th-borders': theme('colors.gray.500'),
+            '--tw-prose-td-borders': theme('colors.gray.500')
+          }
+        }
+      })
     }
   },
-  plugins: [require('@tailwindcss/typography')]
+  plugins: [
+    require('@tailwindcss/typography')
+  ]
 };
